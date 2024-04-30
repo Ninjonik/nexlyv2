69197cc344fafa48a77af37e6dd405d0364988f8
Peter <petricekzatko@gmail.com>
2024-04-30 10:40:05 +0200

    - added background image

--- src/app/components/HomeBase.tsx
+++ src/app/components/HomeBase.tsx
@@ -6,7 +6,10 @@ export const HomeBase = ({ children } : {children: React.ReactNode}) => {
     return (
 
         <main className={"w-full h-full flex flex-col justify-center items-center"}>
-            <div className={"max-h-4/5 w-1/3 bg-base-100 rounded-xl flex flex-col items-center text-center p-8 gap-8"}>
+            <div className={"h-screen w-screen absolute bg-red-500"}>
+                <img src={"/backgroundDark.svg"} alt={"Background image"} className={"w-full h-full object-cover"} />
+            </div>
+            <div className={"max-h-4/5 w-1/3 bg-base-100 rounded-xl flex flex-col items-center text-center p-8 gap-8 z-50"}>
                 <div className={"flex flex-col gap-4 text-center justify-center items-center"}>
                     <Link href="/"><Image src={"/img/nexly.png"} width={150} height={150} alt={"Nexly logo"} /></Link>
                     <h1 className={"text-5xl font-bold"}>Nexly</h1>
