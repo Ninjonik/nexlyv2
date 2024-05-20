c11763820d1794a4be415f29492f42ed7d6c565d
Peter <petricekzatko@gmail.com>
2024-05-19 22:02:11 -0700

    - removed useless import

--- src/app/room/[roomCode]/page.tsx
+++ src/app/room/[roomCode]/page.tsx
@@ -1,6 +1,5 @@
 import {JoinRoomForm} from "@/app/components/JoinRoom/JoinRoomForm";
 import {HomeBase} from "@/app/components/HomeBase";
-import {IndexForm} from "@/app/components/Index/IndexForm";
 import React from "react";
 
 export default function JoinRoom({ params } : { params: {roomCode: string} }) {
